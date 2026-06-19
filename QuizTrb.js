30003f8324ddd36e13a35e0de29fd32323e8fdd5
Alokmustang <deadnesscorp@gmail.com>
2026-06-18 22:38:47 -0300

    Quase finalizado

--- QuizTrb.js
+++ QuizTrb.js
@@ -1,9 +1,40 @@
-function resposta(r) {
-if(r =='A') {
-    
-    
-}
-else{
+function Finalizar() {
 
-}
+    let respostasCorretas = {
+        q1: "B",
+        q2: "C",
+        q3: "C",
+        q4: "B",
+        q5: "B",
+        q6: "D",
+        q7: "B",
+        q8: "C"
+    };
+
+    let acertos = 0;
+
+    for (let pergunta in respostasCorretas) {
+
+        let alternativas = document.getElementsByName(pergunta);
+
+        for (let i = 0; i < alternativas.length; i++) {
+
+            if (
+                alternativas[i].checked &&
+                alternativas[i].value === respostasCorretas[pergunta]
+            ) {
+                acertos++;
+            }
+        }
+    }
+
+    document.getElementById("resultado").innerHTML =
+        "Acertou " + acertos + " de 8 questões!";
+
+    let radios = document.querySelectorAll('input[type="radio"]');
+
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].disabled = true;
+    }
+    document.querySelector(".botao").disabled = true;
 }
